37311ede3578b6590018fae30abf5a543e920e7a
nenadnesta <nenadnesta@gmail.com>
2020-03-30 11:01:26 +0200

    Lazy Loading in Angular.

--- src/app/employee/employee-routing.module.ts
+++ src/app/employee/employee-routing.module.ts
@@ -5,14 +5,9 @@ import { CreateEmployeeComponent } from './create-employee.component';
 import { ListEmployeesComponent } from './list-employees.component';
 
 const appRoutes: Routes = [
-  {
-    path: 'employees',
-    children: [
-      { path: '', component: ListEmployeesComponent },
-      { path: 'create', component: CreateEmployeeComponent },
-      { path: 'edit/:id', component: CreateEmployeeComponent },
-    ]
-  }
+  { path: '', component: ListEmployeesComponent },
+  { path: 'create', component: CreateEmployeeComponent },
+  { path: 'edit/:id', component: CreateEmployeeComponent },
 ];
 
 @NgModule({
