438d547249b216bc0dc2a41545eb24c6ee99fd20
nenadnesta <nenadnesta@gmail.com>
2020-03-30 08:55:23 +0200

    Creating Shared Module in Angular.

--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { CreateEmployeeComponent } from './create-employee.component';
 import { ListEmployeesComponent } from './list-employees.component';
 import { EmployeeRoutingModule } from './employee-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -12,9 +11,8 @@ import { EmployeeRoutingModule } from './employee-routing.module';
     ListEmployeesComponent
   ],
   imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    EmployeeRoutingModule
+    EmployeeRoutingModule,
+    SharedModule
   ]
 })
 export class EmployeeModule { }
